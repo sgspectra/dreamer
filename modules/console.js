ee4ec9e6f77bd1f9c627b2bd29d43000a8f12aae
Andrew Storch <astorch13@gmail.com>
2018-05-18 11:55:40 -0400

    created a module that will be used to pass information to the faux console

---
+++ modules/console.js
@@ -0,0 +1,1 @@
+exports.content = 'What is up great content'
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var con = require('../modules/console.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dreamer'});
+  res.render('index', { title: 'Dreamer', content: con.content});
 });
 
 module.exports = router;
